1cd3c67d19d0c66b154a473900743fac2b089ba1
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 22:01:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
           <div class="item">
               <h3>Home Page</h3>
               <ul>
-                <li><a href='https://github.com/rogerwender/DataScience/blob/main/Prevendo_Fadiga_de_Funcion%C3%A1rios.ipynb'>Prevendo a fadiga de funcionarios</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/Prevendo_Fadiga_de_Funcionarios.ipynb'>Prevendo a fadiga de funcionarios</a></li>
                 <li><a href='#'>Home Page No #2</a></li>
                 <li><a href='#'>Home Page No #3</a></li>
                 <li><a href='#'>Home Page No #4</a></li>
