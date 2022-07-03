913986c911bd742784df743b91cf6711989a0e74
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 22:36:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,16 +10,15 @@
  <![endif]-->
 </head>
 <body>
-	<?php echo "<p>Olá Mundo, Código PHP</p>"; ?>
 <nav>
   <ul class="container">
     <li><a href='#'>Home</a></li>
     <li class='dropdown'>
-      <a href='#'>Pages <i class="fa fa-angle-down"></i></a>
+      <a href='#'>Meus Projetos <i class="fa fa-angle-down"></i></a>
       <div class='mega-menu'>
         <div class="container">
           <div class="item">
-              <h3>Home Page</h3>
+              <h3>Pagina Principal</h3>
               <ul>
                 <li><a href='https://github.com/rogerwender/DataScience/blob/main/Prevendo_Fadiga_de_Funcionarios.ipynb'>Prevendo a fadiga de funcionarios</a></li>
                 <li><a href='https://github.com/rogerwender/DataScience/blob/main/OperadoraTelefone.ipynb'>Verificar Operadora de telefone</a></li>
@@ -29,7 +28,7 @@
               </ul>
           </div> <!-- /.item -->
           <div class="item">
-              <h3>Contact</h3>
+              <h3>Contatos</h3>
               <ul>
                 <li><a href='#'>Contact Page No #1</a></li>
                 <li><a href='#'>Contact Page No #2</a></li>
@@ -54,44 +53,44 @@
       </div><!-- /.mega-menu -->
     </li><!-- /.dropdown -->
     <li class='dropdown'>
-      <a href='#'>Images <i class="fa fa-angle-down"></i> </a>
+      <a href='#'>Imagens <i class="fa fa-angle-down"></i> </a>
       <div class='mega-menu'>
         <div class="container">
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 01</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 02</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 03</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 04</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 05</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
           <div class="item">
-              <h3>Image One</h3>
+              <h3>Imagem 06</h3>
               <img src="http://placehold.it/250x200">
           </div> <!-- /.item -->
           
         </div><!-- .container -->
       </div><!-- .mega-menu-->
     </li><!-- .dropdown -->
-    <li><a href='#'>About</a></li>
-    <li><a href='#'>Conatct</a></li>
+    <li><a href='#'>Sobre</a></li>
+    <li><a href='#'>Contatos</a></li>
   </ul><!-- .container -->
 </nav>
 	
