1bd7184d54cc5fc1185451219464f26269eb2ce4
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 23:14:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 </nav>
 	
 <br><br><br><br><br><br>
-	<img src='PaginaEmConstrucao.png' align = 'center' alt='Site em construção com meus projetos desenvolvidos em Python'>
+	<img src='PaginaEmConstrucao.png'; margin:auto; width:200px>
 	
 
 </body>
