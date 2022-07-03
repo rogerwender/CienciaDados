140c847919e01c68a06965fabdd22db213b2a1bf
rogerwender <58756028+rogerwender@users.noreply.github.com>
2022-07-02 22:13:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
               <h3>Home Page</h3>
               <ul>
                 <li><a href='https://github.com/rogerwender/DataScience/blob/main/Prevendo_Fadiga_de_Funcionarios.ipynb'>Prevendo a fadiga de funcionarios</a></li>
-                <li><a href='DataScience/OperadoraTelefone.ipynb'>Operadora de telefone</a></li>
+                <li><a href='https://github.com/rogerwender/DataScience/blob/main/OperadoraTelefone.ipynb'>Verificar Operadora de telefone</a></li>
                 <li><a href='https://github.com/rogerwender/DataScience/blob/main/GerarQRCode.ipynb'>GerarQRCode</a></li>
                 <li><a href='#'>Home Page No #4</a></li>
 
